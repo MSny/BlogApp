80833052e89085e6e057bf26b57cc5932e39f84c
Meir <meir@Meirs-Mac.local>
2015-04-13 19:56:18 -0400

    index and new pages for server

--- blogServer.js
+++ blogServer.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var sql = require('sqlite3').verbose;
+var db = new sqlite3.Database('blog.db')
 var ejs = require("ejs");
 //Designate where ejs files are kept
 app.set("view_engine", "ejs");
@@ -13,6 +14,34 @@ app.use(metOverride('_method'));
 
 var port = 3000;
 
+app.get('/', function(req, res){
+	res.redirect('blog')
+});
+
+//page to view all posts
+app.get('/blog', function(req, res){
+	db.all("SELECT * FROM posts", function(err, data) {
+		if (err) {
+			console.log(err)
+		} else {
+			var posts = data;
+		} res.render('index.ejs', {posts: posts})
+	})	
+})
+
+// individual posts
+app.get('/blog/:id', function(req, res){
+	var id = req.params.id
+	db.get('SELECT * FROM posts WHERE id = ?', id, function(err, data){
+		item = data
+		res.render('show.ejs', {thisBlog: item})
+	})
+})
+
+//new posts page
+app.get('/blog/new', function(req, res){
+	res.render('new.ejs')
+});
 
 //liten to server
 app.listen(3000);
